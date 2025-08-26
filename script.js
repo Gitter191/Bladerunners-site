d378dbc58e90cc1db44885717d7a7d8fc7bd32fd
Sef-Roercollege <Soml106998@soml.nl>
2025-08-26 10:40:09 +0200

    New

--- script.js
+++ script.js
@@ -31,3 +31,18 @@ form.addEventListener("submit", async (e) => {
     formStatus.innerText = "Verbindingsfout, probeer later opnieuw.";
   }
 });
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
